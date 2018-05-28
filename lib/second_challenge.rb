6682f4832326f3a96817f7e0b1c4506ee2a6f90f
Aziz Kosber <a.kosber@gmail.com>
2018-05-28 15:41:54 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_freinds => []
+      :hero_friends => []
       },
     :capulet => {
       :patriarch => {},
